6131f8b60e6ace4a18af2a2431e89ead9883eb07
Nikhil Jindal <nikhil.bits02@gmail.com>
2020-04-21 16:51:23 +0530

    Updates

---
+++ precache-manifest.f652ac60e98b2aab2b371ad57e007ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3616620405baeae9d8895366ce4841f6",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "22476ea19f9ef401f202",
+    "url": "/react-deploy/static/css/main.fe8a8d18.chunk.css"
+  },
+  {
+    "revision": "1dba67e73772b88851d7",
+    "url": "/react-deploy/static/js/2.d05ebe70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d05ebe70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22476ea19f9ef401f202",
+    "url": "/react-deploy/static/js/main.2eb81db7.chunk.js"
+  },
+  {
+    "revision": "127c77060fc6be485e1c",
+    "url": "/react-deploy/static/js/runtime-main.064f848b.js"
+  }
+]);
